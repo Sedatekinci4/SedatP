35ef94f93f9815b15360dcccdb4cf2eb7d004629
Sedat Ekinci <sedatekinci425@gmail.com>
2022-08-04 17:29:04 +0300

    Add files via upload

---
+++ PrimeNext/main.c
@@ -0,0 +1,29 @@
+// Ýnputted Next Prime Number
+
+#include <stdio.h>
+
+int main(){
+    char answer = 'y';
+    int num = 2, i = 0;
+    printf("\This program will find the next prime number after your inputted integer.\n");
+    printf("Please enter your integer: \t");
+    scanf("%d", &num);
+    do
+    {
+        num++;
+        printf("Do you want to find the next prime number? [ y / n ] ----->  ");
+        scanf(" %c", &answer);
+
+        for(i = 2; i < num; i++)
+        {
+            if(num % i == 0)
+            {
+                num++;
+            }
+        }
+        printf("The next Prime number is ----> %d\n", num);
+    }while(answer == 'y' || answer == 'Y');
+
+    return 0;
+}
+
