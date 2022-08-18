8e683e1e62933c057f15bf29cec6a7d39cf037a5
Sedat Ekinci <sedatekinci425@gmail.com>
2022-08-18 20:14:44 +0300

    Add files via upload

---
+++ PrintsthWithoutsemicolon/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    if(printf("Hello world without semicolon with if statement!!!!!\n")){}
+    switch(printf("Hello world without semicolon with switch case!!!!!\n")){}
+    while(!printf("Hello world without semicolon with while loop!!!!!\n")){}
+    return 0;
+}
