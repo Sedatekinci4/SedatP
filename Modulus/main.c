0e4ad2cf7d37aa0f976d2ec5f53fd61459e413aa
Sedat Ekinci <sedatekinci425@gmail.com>
2022-08-28 15:41:24 +0300

    Add files via upload

---
+++ Modulus/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("%f\n", fmod(8.3,5)); // How to use modulo operator with floating numbers
+
+    printf("%d\n", 7%5); // How to use it with integers
+
+    return 0;
+}
